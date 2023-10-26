d1b6b16c21eb49e25ae8604a938f81c3b72a2b9d
dnstroupe <141791480+dnstroupe@users.noreply.github.com>
2023-10-25 20:53:44 -1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const messageDiv = document.getElementById("message");
-    messageDiv.textContent = "Hello from the Naruto World!";
+    if (messageDiv) {
+        messageDiv.textContent = "Hello from the Naruto World!";
+    }
 });
 
 console.log("Hello from the Naruto World!");
-
